8dd00c5d6e4aa47a69d73e75201fdf6d2a60e8fd
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2021-10-27 15:18:42 +0200

    Basic typescript project setup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # FLEET-simulator
+
+This project has been setup using Typescript, Node@v16 and Yarn.
+
+To get started run `yarn` to install dependencies, and `yarn start` to run the `main.ts` file.
---
+++ main.ts
@@ -0,0 +1,5 @@
+// Copyright (c) the FLEET project. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information
+
+console.log('Hello World!');
+
