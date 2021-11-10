4d03ca625e152daaeef31720a681cf923ce43ef3
Hui Song <hui.song@sintef.no>
2021-11-10 13:11:45 +0100

    initial interfaces

---
+++ model/IfcDeployment.ts
@@ -0,0 +1,35 @@
+export interface Artefact{
+    id: string;
+    config: object;
+    developer: Developer;
+}
+
+export interface Version{
+    name: string;
+    releaseType: "major" | "minor" | "bug fixing";
+    artefact: Artefact;
+    config: object;
+}
+
+export interface Deployment{
+    id: string;
+    number: number;
+    runtime: Version;
+    microService: Version[];
+    config: object;
+}
+
+export interface Developer{
+    name: string;
+}
+
+export interface Env{
+    config: object;
+}
+
+
+
+
+
+
+
